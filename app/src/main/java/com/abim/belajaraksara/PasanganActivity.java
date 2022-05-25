4b0cb1bd7ec5a101986e1731cb3aedfb2a424eb5
MaulanaMalikIbrahim <abhiemsmile@gmail.com>
2022-05-25 20:47:11 +0700

    Add Aksara Pasangan Feature

--- app/src/main/java/com/abim/belajaraksara/PasanganActivity.java
+++ app/src/main/java/com/abim/belajaraksara/PasanganActivity.java
@@ -2,13 +2,24 @@ package com.abim.belajaraksara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
-public class PasanganActivity extends AppCompatActivity {
+public class PasanganActivity extends AppCompatActivity implements View.OnClickListener {
+    Context ctx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pasangan);
+        getSupportActionBar().setTitle("Aksara Pasangan");
+
+        ctx = this;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
     }
 }
