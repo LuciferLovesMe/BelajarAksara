5085f081a7621298046bf881fc5e865521e4d2e5
MaulanaMalikIbrahim <abhiemsmile@gmail.com>
2022-05-25 20:13:09 +0700

    Add Aksara Sandhangan Feature

--- app/src/main/java/com/abim/belajaraksara/SandhanganActivity.java
+++ app/src/main/java/com/abim/belajaraksara/SandhanganActivity.java
@@ -2,13 +2,24 @@ package com.abim.belajaraksara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
-public class SandhanganActivity extends AppCompatActivity {
+public class SandhanganActivity extends AppCompatActivity implements View.OnClickListener {
+    Context ctx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sandhangan);
+        getSupportActionBar().setTitle("Sandhangan");
+
+        ctx = this;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
